b12ad33fc6b85589d2c262fd78155ca5d37d0d6a
Giovanna Lanzillotta <lanzillottagigi@gmail.com>
2025-05-01 16:15:08 -0300

    enviando contagem_regressiva

---
+++ contagem_regressiva/contagem.py
@@ -0,0 +1,14 @@
+# importação da biblioteca time
+import time
+
+# Pede ao usuário para escolher um número para comerçãr a contagem regressiva
+contador = int(input("Digite um número para a contagem regressiva: "))
+
+# usando While, Enquanto o contador for maior que 10,ele faz a contagem com decremento
+while contador > 0:
+    print(f"⏰ {contador}")
+    contador -= 1
+    time.sleep(1) # Faz a contagem parar no 1
+
+#Depois que o while acabar mostra a mensagem de fim
+print("💫 fim!")
